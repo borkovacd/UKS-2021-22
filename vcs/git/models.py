60549a4a1a035aa80d9e11e6745f7fd42443f425
Dragan Borkovac <borkovac.dragan@uns.ac.rs>
2022-07-31 11:28:24 +0200

    Implement (or update) Milestone CRUD operations

--- vcs/git/models.py
+++ vcs/git/models.py
@@ -38,17 +38,20 @@ class Project(models.Model):
 
 class Milestone(models.Model):
     title = models.CharField(max_length=200, blank=False)
-    due_date = models.DateTimeField()
+    due_date = models.DateTimeField(null=True, blank=True)
     description = models.TextField(max_length=300, blank=True)
     state = models.CharField(
         max_length=6,
         choices=GENERAL_STATES,
         default='OPEN',
     )
-    project = models.ForeignKey(Project, default=1, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.title)
+
+    def get_absolute_url(self):
+        return reverse('project-detail', kwargs={'pk': self.project.pk})
 
 
 class Label(models.Model):
@@ -73,7 +76,7 @@ class Issue(models.Model):
     date_closed = models.DateTimeField(null=True)
     is_open = models.BooleanField(default=True)
     milestone = models.ForeignKey(
-        Milestone, on_delete=models.CASCADE, blank=True, null=True)
+        Milestone, on_delete=models.SET_NULL, blank=True, null=True)
     labels = models.ManyToManyField(Label, blank=True)
     assignees = models.ManyToManyField(
         User, related_name='assignees', blank=True)
