dfe02c55542f67a2509456b53b7c0891c8728f89
Dragan Borkovac <borkovac.dragan@uns.ac.rs>
2022-08-21 10:07:56 +0200

    Add Static Root

--- vcs/vcs/settings.py
+++ vcs/vcs/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
